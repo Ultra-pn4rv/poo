c0658e815105672e61d6a05652e6e13aad0c372c
Ultra-pn4rv <tsarultra@gmail.com>
2024-09-23 09:52:43 -0400

    herança polimorfismo encapsulamento js

---
+++ 16-heranca-polimorfismo-encapsulamento.js
@@ -0,0 +1,77 @@
+// Classe base Personagem
+class Personagem {
+  // Encapsulando os atributos com #, tornando-os privados.
+  #nome;
+  #classe;
+  #nivel;
+  #vida;
+  #mana;
+
+  // Método Contrutor
+  constructor(nome, classe, nivel, vida, mana) {
+    this.#nome = nome;
+    this.#classe = classe;
+    this.#nivel = nivel;
+    this.#vida = vida;
+    this.#mana = mana;
+  }
+  // Métodos Getters e Setters
+  // Métodos Getter e Setter para o atributo nome
+  get nome() {
+    return this.#nome;
+  }
+  set nome(nome) {
+    this.#nome = nome;
+  }
+  // Métodos Getters e Setter para o atributo classe
+  get classe() {
+    return this.#classe;
+  }
+  set classe(classe) {
+    this.#classe = classe;
+  }
+  // Métodos Getters e Setter para o atributo nivel
+  get nivel() {
+    return this.#nivel;
+  }
+  set nivel(nivel) {
+    this.#nivel = nivel;
+  }
+  // Métodos Getters e Setter para o atributo vida
+  get vida() {
+    return this.#vida;
+  }
+  set vida(vida) {
+    this.#vida = vida;
+  }
+  // Métodos Getters e Setters para o atributo mana
+  get mana() {
+    return this.#mana;
+  }
+  set mana(mana) {
+    this.#mana = mana;
+  }
+
+  // Métodos
+  // Simulando sobrecarga com rest parameter
+  atacar(...args) {
+      if (args.length === 0) {
+          console.log(`{$this.#nome} realizou um ataque normal!`);
+        } else if (args.length === 1) {
+            console.log(`${this.#nome} atacou com um poder de ${args[0]}!`);
+        } else if (args.length === 2) {
+            console.log(`${this.#nome} usou ${args[1]} e atacou com ${args[0]} de poder!`);
+        } else {
+            console.log("Número inválido de argumentos.");
+        }
+    }
+    defesa() {
+        console.log(`${this.#nome} defendeu com ${this.#nivel * 2} pontos de defesa!`);
+    }
+    
+    receberDano(dano) {
+        this.#vida -= dano;
+        console.log(`${this.#nome} recebeu ${dano} de dano! Vida restante: ${this.#vida}`);
+    }
+}
+
